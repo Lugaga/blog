c285f4aedb91879792a5ce894cb031e0d9f10f77
Lugaga <maureezgaga@gmail.com>
2019-07-07 18:13:26 +0300

    add migrartions

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME='maureezgaga@gmail.com'
+export MAIL_PASSWORD='biologybaba!'
+export DATABASE_URL='postgres://niploulydylefr:2221bcc015016020f8e471877bb960563148ead31696ee3341e9a51fc986eeed@ec2-54-221-237-246.compute-1.amazonaws.com:5432/ddhq2fgfqg3k4m'
+
+
+python3.6 manage.py server
